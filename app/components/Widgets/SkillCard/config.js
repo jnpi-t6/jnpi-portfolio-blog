d2424076aa25aab92915e011ea9a5dea5ccf3fba
jnpi <jnpimailbox@gmail.com>
2024-03-27 17:06:43 +0900

    [feat]スキルカード、スキルアイコンリストを作成

---
+++ app/components/Widgets/SkillCard/config.js
@@ -0,0 +1,51 @@
+export const skills = [
+  {
+    label: "HTML5",
+    icon: "HTML5Icon",
+    description:
+      "ブロックチェーンのDapps（分散アプリ）に触れて、フロントエンドの面白さと楽しさに感動しました。",
+  },
+  {
+    label: "CSS",
+    icon: "CSSIcon",
+    description:
+      "CSSの設計は、デザインにとって大事な「統一感」や「バランス」がそのまま反映される言語だと思います。",
+  },
+  {
+    label: "JavaScript",
+    icon: "JavaScriptIcon",
+    description:
+      "初めて学んだ言語です。ブロックチェーンでスナイプボットを作りたくて学び始めたため、DOM操作を知るより先に、APIを叩いて、Node.jsでプログラムを動かす操作を覚えました。",
+  },
+  {
+    label: "React",
+    icon: "ReactIcon",
+    description:
+      "Reactの分からない部分は、たいていJavaScriptの部分だと思います。innerHTMLを直接触らずに済んだり、とても便利だと思います。",
+  },
+  {
+    label: "Nextjs",
+    icon: "NextjsIcon",
+    description:
+      "SSG（静的サイトジェネレーター）として使い始めました（いわゆるJamstackの編成の一環として）。これでブログを作って、MicroCMSで投稿管理、NetlifyかVerselにデプロイ、が個人的にやりやすく感じました。",
+  },
+
+  {
+    label: "Github",
+    icon: "GithubIcon",
+    description:
+      "init, add, commit, push, branch, checkout, merge, その他諸々。ブロックチェーンみたいにハッシュで管理しているらしいです。自分は個人開発を始めた当初、Gitの存在を知らずにvscode上の履歴を全消しした経験があります。事前に勉強しましょう。",
+  },
+  {
+    label: "Netlify",
+    icon: "NetlifyIcon",
+    description:
+      "ホスティングサービス。Verselでも楽ですが、Jamstackの提唱者が作ったものということで、リスペクトを込めて使用。管理画面も分かりやすく使いやすいと思います。",
+  },
+  {
+    label: "StyledComponents",
+    icon: "StyledComponentsIcon",
+    description:
+      "他のCSS-in-JS同様、同一ファイル上でコンパクトにcssを書けるし、何よりpropsをcssに投げられるのが便利。TailwindCSSのほうが人気なようですが、海外のサイトではよく使用されているような気がします。",
+  },
+];
