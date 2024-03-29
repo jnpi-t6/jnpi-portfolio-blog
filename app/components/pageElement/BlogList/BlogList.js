a69e2f40a00248e47aa2323810e72555235b4bee
jnpi <jnpimailbox@gmail.com>
2024-03-29 17:00:38 +0900

    [feat]API取得コード、ブログ記事一覧コンポーネント作成

---
+++ app/components/pageElement/BlogList/BlogList.js
@@ -0,0 +1,54 @@
+import styles from "./BlogList.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+export default async function BlogList({ contents }) {
+  return (
+    <>
+      <div className={styles.postslayout}>
+        {contents?.map((post) => (
+          <Link
+            key={post.id}
+            href={`/blog/${post.id}`}
+            className={styles.blogposts}
+          >
+            {post.eyecatch ? (
+              <div className={styles.eyecatch}>
+                <Image
+                  src={post.eyecatch.url}
+                  alt={post.title}
+                  width={400}
+                  height={300}
+                  sizes="100vw"
+                  style={{
+                    objectFit: "cover",
+                    width: "100px",
+                    height: "auto",
+                  }}
+                />
+              </div>
+            ) : (
+              <div className={styles.defaultEyecatch}>
+                <Image
+                  src="/bookspark.png"
+                  alt="blog-image"
+                  width={400}
+                  height={300}
+                  sizes="100vw"
+                  style={{
+                    objectFit: "cover",
+                    width: "100px",
+                    height: "auto",
+                  }}
+                />
+              </div>
+            )}
+            <p className={styles.title}>{post.title}</p>
+
+            <div className={styles.darkcover}></div>
+          </Link>
+        ))}
+      </div>
+    </>
+  );
+}
