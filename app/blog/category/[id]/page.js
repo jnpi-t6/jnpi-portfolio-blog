c6da032ad7719eca449e456d7081dcac0daee383
jnpi <jnpimailbox@gmail.com>
2024-03-29 17:51:32 +0900

    [feat]ブログ記事ページ、カテゴリーページ作成

---
+++ app/blog/category/[id]/page.js
@@ -0,0 +1,43 @@
+import styles from "./categoryPage.module.css";
+import { notFound } from "next/navigation";
+import { getCategoryPosts, getCategories, getCategoryId } from "@/lib/api";
+import BlogList from "@/app/components/pageElement/BlogList/BlogList";
+
+export async function generateStaticParams() {
+  const categoryData = await getCategories();
+  const paths = categoryData.contents.map((cat) => {
+    return {
+      id: cat.id,
+    };
+  });
+  return paths;
+}
+
+export default async function CategoryPage({ params: { id } }) {
+  const { contents } = await getCategoryPosts(id);
+
+  const catData = await getCategoryId(id);
+
+  const catName = catData.contents.map((cat) => {
+    return {
+      name: cat.name,
+    };
+  });
+
+  if (!contents) {
+    notFound();
+  }
+
+  return (
+    <>
+      <div className={styles.container}>
+        {catName?.map((cat) => (
+          <h2 key={cat.id} className={styles.title}>
+            {cat.name}
+          </h2>
+        ))}
+        <BlogList contents={contents} />
+      </div>
+    </>
+  );
+}
