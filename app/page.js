555bb312bcf007b16626f0e05d96906a3cbfae3b
jnpi <jnpimailbox@gmail.com>
2024-03-29 18:44:49 +0900

    [feat]トップページにコンポーネントを配置

--- app/page.js
+++ app/page.js
@@ -1,3 +1,15 @@
+import BlogPosts from "./components/widgets/BlogPosts/BlogPosts";
+import IntroCard from "./components/widgets/IntroCard/IntroCard";
+import SkillCard from "./components/widgets/SkillCard/SkillCard";
+import SkillIconList from "./components/widgets/SkillIconList/SkillIconList";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <SkillIconList />
+      <SkillCard />
+      <IntroCard />
+      <BlogPosts />
+    </>
+  );
 }
