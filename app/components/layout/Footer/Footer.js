9a750e3ff21b1525ee7f492107f9727bb4343381
jnpi <jnpimailbox@gmail.com>
2024-04-10 18:02:36 +0900

    [feat]footerにgithubのリンク追加

--- app/components/layout/Footer/Footer.js
+++ app/components/layout/Footer/Footer.js
@@ -1,10 +1,18 @@
 import styles from "./Footer.module.css";
+import { GithubIcon } from "../../widgets/SkillCard/icons";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <>
       <footer className={styles.container}>
-        <p>© jnpi Portfolio Blog</p>
+        <p className={styles.sitename}>© jnpi Portfolio Blog</p>
+        <Link
+          href="https://github.com/jnpi-t6/jnpi-portfolio-blog"
+          className={styles.githublink}
+        >
+          <GithubIcon width="30" />
+        </Link>
       </footer>
     </>
   );
